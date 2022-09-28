b50e124915100b8812802f6a44f7787e621a279b
Danil <Kutfilloev.danil@yandex.ru>
2022-09-28 10:51:47 +0500

    напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N(через факториал)

--- Task002.py
+++ Task002.py
@@ -2,3 +2,11 @@
 #1 - 1 * 1, 2 - 1 * 2, 3 - 1 * 2 * 3, 4 - 1 * 2 * 3 * 4 и т.д.
 #- 4 -> [1, 2, 6, 24]
 #- 6 -> [1, 2, 6, 24, 120, 720]
+
+n = int(input())
+ 
+factorial = 1
+for i in range(1, n+1):
+    factorial *= i
+    print(factorial, end=" ")
+
